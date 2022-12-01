0366fb6b25eded4a0ced4a45b6c3484cc91a6a18
Olabisi.Adekoya <gloryolabisi.go@gmail.com>
2022-11-30 22:57:20 -0800

    modified:   Nfts.html 	modified:   dashboard.html 	new file:   images/search icon.png 	modified:   index.html 	modified:   script.js 	modified:   styles.css 	modified:   swap.html

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const container = document.querySelector(".container")
+/*const container = document.querySelector(".container")
 const popup = document.querySelector(".popup")
 
 
 function toggle() {
     container.classList.toggle("container-active")
     popup.classList.toggle("popup-active")
-}
+}*/
  
+document.querySelector(".popup-btn").addEventListener("click", function(){
+    document.body.classList.add("active-popup");
+})
+document.querySelector(".nav-item2").addEventListener("click", function(){
+    document.body.classList.add("active-popup");
+})
+
+document.querySelector(".popup .close-btn").addEventListener("click", function(){
+    document.body.classList.remove("active-popup");
+})
